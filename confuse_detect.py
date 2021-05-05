3f64640259d1d934c431fc5fd17db44ccc27f4c8
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-05-05 01:02:49 -0700

    Added roi using slice function

--- confuse_detect.py
+++ confuse_detect.py
@@ -7,13 +7,13 @@ import numpy as np
 
 # in paint, the coordinates are displayed as cols, rows
 
-roi_status_effect_rows = (597,616)
-roi_status_effect_cols = (738,822)
+roi_status_effect_rows = slice(597,616)
+roi_status_effect_cols = slice(738,822)
 
 img = cv2.imread('./confuse_img_1.png')
 
 # roi_status_effect = img[roi_status_effect_rows[0]:roi_status_effect_rows[1], roi_status_effect_cols[0]:roi_status_effect_cols[1]]
-roi_status_effect = img[597:616, 738:822]
+roi_status_effect = img[roi_status_effect_rows, roi_status_effect_cols]
 cv2.imshow('Status effect area', roi_status_effect)
 
 cv2.waitKey(0)
